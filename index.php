d5884237898dfa4ed7ba9d8a54eeab5570913b82
Naiyan Noor <47422181+NaiyanNoor@users.noreply.github.com>
2022-08-25 08:58:03 +0800

    initial Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$name = 10;
+echo gettype($name);
+$name = "nirjhor";
+echo "<br>";
+echo gettype($name);
+
+$name = [0, 1, 2, 3, 4, 23, 343, 23, 2, 2, 2];
+echo "<br>";
+echo gettype($name);
+
+// print_r($name);
+
+// echo "Welcome";
+echo "<br>Welcone, " . $name[2] . "<br>";
+echo "Welcone, " . '<h1>' . $name[0] . '</h1>';
+?>
+
+
+<ul>
+    <?php
+        for ($i = 0; $i < count($name); $i++) {
+            echo "<li>$name[$i]</li>";
+        }
+    ?>
+    
+</ul>
+
+
+
